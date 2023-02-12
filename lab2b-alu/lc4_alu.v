b481d6f3606485aefb1e85d7ca21f3448a9dd13e
Zihao Wang <zihaow1@big06.seas.upenn.edu>
2023-02-12 17:50:31 -0500

    final

--- lab2b-alu/lc4_alu.v
+++ lab2b-alu/lc4_alu.v
@@ -9,31 +9,29 @@ module lc4_alu(input  wire [15:0] i_insn,
                input wire [15:0]  i_r1data,
                input wire [15:0]  i_r2data,
                output wire [15:0] o_result);
-  
+  reg  [15:0] pc;
   reg  [15:0] result;
+  reg  [15:0] dmem [0:1023];
   wire [3:0] opcode;
   wire [2:0] type;
   wire [1:0] type2;
   wire [3:0] shift;
   wire [8:0] imm9;
   wire [7:0] uimm8;
+  wire [5:0] imm6;
+  wire [6:0] imm7;
+  wire [1:0] type3;
+
   assign opcode = i_insn[15:12];
   assign type = i_insn[5:3];
   assign type2 = i_insn[5:4];
   assign shift = i_insn[3:0];
   assign imm9 = i_insn[8:0];
   assign uimm8 = i_insn[7:0];
+  assign imm6 = i_insn[5:0];
+  assign imm7 = i_insn[6:0];
+  assign type3 = i_insn[8:7];
   parameter constant = 16'h00FF;
-  reg [15:0] remainder;
-  reg [15:0] quotient;
-  reg [15:0] rst;
-  reg [15:0] temp_divisor;
-  reg [15:0] bit;
-
-  integer i;
-  reg [15:0] sum;
-  reg [15:0] carry;
-  reg [15:0] diff;
    
   always @* begin
     case (opcode)
@@ -48,9 +46,9 @@ module lc4_alu(input  wire [15:0] i_insn,
       4'b0001: if (type == 3'b001)
         result = i_r1data * i_r2data;
       4'b0001: if (type == 3'b011)
-        result = 0;
+        result = 16'h0000;
       4'b0001: if (type == 3'b000)
-        result = 0;
+        result = 16'h0000;
       4'b1010: if (type2 == 2'b00)
         result = i_r1data << shift;
       4'b1010: if (type2 == 2'b01)
@@ -58,11 +56,24 @@ module lc4_alu(input  wire [15:0] i_insn,
       4'b1010: if (type2 == 2'b10)
         result = i_r1data >> shift;
       4'b1001: 
-        result = {1'b0,imm9[8:0]};
+        result = {imm9[8] ? 16'hFFFF : 16'h0000, imm9[7:0]};
       4'b1101:
-        result = (i_r1data & constant)|(uimm8 << 8);  
+        result = (i_r1data & constant)|(uimm8 << 8);
+      4'b0010: 
+        result = 16'h0001;
+      4'b0000:
+        result = i_pc+1;
+      4'b1100:
+        result = i_r1data;
+      4'b0100:
+        result = i_r1data;
+      4'b1111:
+        result = i_pc+1;
+      4'b1000:
+        result = i_r1data;         
         default: result = 16'h0000;
     endcase
   end           
   assign o_result = result;
+  assign i_pc = pc;
 endmodule
